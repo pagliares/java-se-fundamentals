cbe5d1a86dfb881b920521cd6d71579a37241f76
Rodrigo Martins Pagliares <pagliares@gmail.com>
2022-06-02 18:03:28 -0300

    Examples of episode 6

---
+++ src/main/java/episodio6/TesteHospede.java
@@ -0,0 +1,33 @@
+package episodio6;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class TesteHospede {
+    public static void main(String[] args) {
+        Hospede rodrigo = new Hospede();
+        rodrigo.setNome("Rodrigo");
+        rodrigo.setSobrenome("Martins Pagliares");
+
+        // exibindo o estado do objeto
+        System.out.println("Nome...: " + rodrigo.getNome());
+        System.out.println("Sobrenome..: " + rodrigo.getSobrenome());
+
+        Hospede mariana = new Hospede();
+        mariana.setNome("Mariana");
+        mariana.setSobrenome("Pereira");
+
+        // exibindo o estado do objeto
+        System.out.println("Nome...: " + mariana.getNome());
+        System.out.println("Sobrenome..: " + mariana.getSobrenome());
+
+        Date data = new Date();
+        ArrayList lista = new ArrayList();
+
+        Hospede francisco = new Hospede();
+        francisco.setNome("Francisco");
+        francisco.setSobrenome("Ribeiro");
+
+
+    }
+}
