2e40f411f48a46fe3512ee5a18ade8427aba626f
Rodrigo Martins Pagliares <pagliares@gmail.com>
2022-06-02 11:03:51 -0300

    Examples of episode 3

---
+++ src/main/java/episodio3/Hospede.java
@@ -0,0 +1,32 @@
+package episodio3;
+
+public class Hospede {
+    private String nome; // campo, variável de instância, atributo
+    private String sobrenome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public static void main(String[] args) {
+        Hospede rodrigo = new Hospede();
+        rodrigo.setNome("Rodrigo");
+        rodrigo.setSobrenome("Martins Pagliares");
+
+        // exibindo o estado do objeto
+        System.out.println("Nome...: " + rodrigo.getNome());
+        System.out.println("Sobrenome..: " + rodrigo.getSobrenome());
+    }
+}
---
+++ src/main/java/episodio3/TesteHospede.java
@@ -0,0 +1,21 @@
+package episodio3;
+
+public class TesteHospede {
+    public static void main(String[] args) {
+        Hospede rodrigo = new Hospede();
+        rodrigo.setNome("Rodrigo");
+        rodrigo.setSobrenome("Martins Pagliares");
+
+        // exibindo o estado do objeto
+        System.out.println("Nome...: " + rodrigo.getNome());
+        System.out.println("Sobrenome..: " + rodrigo.getSobrenome());
+
+        Hospede mariana = new Hospede();
+        mariana.setNome("Mariana");
+        mariana.setSobrenome("Pereira");
+
+        // exibindo o estado do objeto
+        System.out.println("Nome...: " + mariana.getNome());
+        System.out.println("Sobrenome..: " + mariana.getSobrenome());
+    }
+}
