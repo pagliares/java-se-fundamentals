ee8dbfdf515195d551e1362cf2fe58e7ac586ccd
Rodrigo Martins Pagliares <pagliares@gmail.com>
2022-06-01 23:07:57 -0300

    Examples for the first episode

---
+++ src/main/java/episodio1/Hospede.java
@@ -0,0 +1,22 @@
+package episodio1;
+
+public class Hospede {
+    private String nome; // campo, variável de instância, atributo
+    private String sobrenome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+}
